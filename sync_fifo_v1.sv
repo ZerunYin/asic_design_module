8c5b9c7b79297adb9b7b099f0d6969de9fa55d84
Yinzerun <zerunyin@outlook.com>
2022-02-25 01:13:18 +0800

    sync FIFO
    initial version of sync FIFO

---
+++ sync_fifo_v1.sv
@@ -0,0 +1,53 @@
+module sync_fifo_v1 #(
+    parameter      DEPTH = 2,
+    parameter type T     = logic
+)(
+    input    clk,
+    input    rst_n,
+
+    input    wen,
+    input  T data_in,
+
+    input    ren,
+    output T data_out,
+
+    output   full,
+    output   empty
+);
+
+localparam ADDR_SIZE = $clog2(DEPTH);
+
+logic [ADDR_SIZE-1:0] waddr;
+logic                 wwrap;
+logic [ADDR_SIZE-1:0] raddr;
+logic                 rwrap;
+
+T mem [DEPTH-1:0];
+always_ff @(posedge clk) begin
+    if (wen) begin
+        mem [waddr] <= data_in;
+    end
+end
+
+assign data_out = mem [raddr];
+
+always_ff @(posedge clk, negedge rst_n) begin
+    if (!rst_n) begin
+        {wwrap, waddr} <= '0;
+    end else if (wen) begin
+        {wwrap, waddr} <= {wwrap, waddr} + 1'b1;
+    end
+end
+
+always_ff @(posedge clk, negedge rst_n) begin
+    if (!rst_n) begin
+        {rwrap, raddr} <= '0;
+    end else if (ren) begin
+        {rwrap, raddr} <= {rwrap, raddr} + 1'b1;
+    end
+end
+
+assign full  = (wwrap!=rwrap) & (waddr == raddr);
+assign empty = (wwrap==rwrap) & (waddr == raddr);
+
+endmodule
